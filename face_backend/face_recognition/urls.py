4e34c47f10b3f4b216458da23ca1d30ad22f0045
phamquan1a <23020418@vnu.edu.vn>
2025-05-25 17:26:53 +0700

    Initial commit: upload full project

--- face_backend/face_recognition/urls.py
+++ face_backend/face_recognition/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import recognize_user
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
 
     path('recognize', recognize_user),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
